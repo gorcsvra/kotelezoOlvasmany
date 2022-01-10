6986dd3c47ff688fecacf61ba442b46116460b4a
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2022-01-10 14:27:42 +0100

    a nappali éjszakai mód kapcsolóstól működik, most kiteszem a netlify-re

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const themeSwitcher = document.getElementById("switch-theme");
-function csere() {
-  const selectBody = document.body.classList.toggle("active");
+
+
+/*light/night kapcsoló*/
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
 }
-themeSwitcher.addEventListener("click", csere);
+
+// When toggle is switched, check local storage for the currently stored theme, and switch it to the opposite
+
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-light');
+  } else {
+      setTheme('theme-dark');
+  }
+}
+
+// Invoke function on loading page - check storage if a previous theme was stored, if not, load default (light) theme
+
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+  } else {
+      setTheme('theme-light');
+  }
+})();
